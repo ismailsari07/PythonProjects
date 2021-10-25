781a973e6338c51fda319484d9036b63628816c6
ismailsari07 <93125401+ismailsari07@users.noreply.github.com>
2021-10-26 01:59:59 +0300

    Update numberGuessing.py

--- numberGuessing.py
+++ numberGuessing.py
@@ -1,16 +1,46 @@
 import random
-sayi = random.randint(0, 10)
+
+top_of_range = input('Aklimdan tutabilecegim en buyuk sayiyi gir: ')
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    sayi = random.randint(0, top_of_range)
+    if top_of_range <=0:
+        print('Lutfen bir daha kine pozitif sayi girmeyi dene.')
+        quit()
+else:
+    print('Lutfen bir daha kine sayi girmeyi dene')
+    quit()
+
 oyundurumu = 1
 print("*****Sayi Tahmin Etme Oyununa Hosgeldin*****")
+
 while (oyundurumu == 1):
-    tahmin = int(input('Tahmin ettigin sayiyi gir : '))
+    while True:
+        tahmin = input('Tahmin ettigin sayiyi gir : ')
+        if tahmin.isdigit():
+            tahmin = int(tahmin)
+            if tahmin <= -1:
+                print('Lutfen pozitif bir sayi gir')
+                continue
+            break
+        else:
+            print('Lutfen bir sayi gir: ')
+            
     if (tahmin == sayi):
-        oyundurumu = int(input('Tebrikler sayiyi buldun\nOyuna devam etmek istermisin (1 : 0)'))
-        if (oyundurumu == 1):
-            print('Aklimda yeni bir sayi tutuyorum 1 saniye.\nTamam tuttum;')
-            sayi = random.randint(0, 10)
-        else :
-            print('Tekrar gorusmek uzere dostum, kendine iyi bak.')
+        print('tebrikler sayiyi buldun')
+        while True:
+            oyundurumu = input('\nOyuna devam etmek istermisin (1 : 0)')
+            if oyundurumu != "1" and oyundurumu != "0":
+                print('Lutfen 1 veya 0 degerini gir dostum (1/0): ')
+                continue
+            else:
+                oyundurumu = int(oyundurumu)
+            if (oyundurumu == 1):
+                print('Aklimda yeni bir sayi tutuyorum 1 saniye.\nTamam tuttum;')
+                sayi = random.randint(0, top_of_range)
+            else :
+                print('Tekrar gorusmek uzere dostum, kendine iyi bak.')
+            break
     elif tahmin > sayi:
         print('Biraz daha asagilara dostum.')
     elif tahmin < sayi:
