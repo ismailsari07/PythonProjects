99665f84b0768a8137f9d9c7d77b0f68a9add1ee
ismailsari07 <93125401+ismailsari07@users.noreply.github.com>
2021-10-26 20:25:25 +0300

    Update dice-rolling-simulator.py

--- dice-rolling-simulator.py
+++ dice-rolling-simulator.py
@@ -1,6 +1,7 @@
 import random
 print('*****Zar Atma Oyununa Hosgeldiniz*****')
 oyundurumu = 1;
+
 while (oyundurumu == 1):
     zarNumber = random.randint(1,6)
     print('Zar ', zarNumber, "'i gosteriyor")
