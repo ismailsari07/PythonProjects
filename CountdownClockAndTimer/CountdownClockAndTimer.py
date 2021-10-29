290da1caea4ff66da353afb28333b935e0db292d
ismailsari07 <93125401+ismailsari07@users.noreply.github.com>
2021-10-29 07:58:17 +0300

    Add files via upload

---
+++ CountdownClockAndTimer/CountdownClockAndTimer.py
@@ -0,0 +1,33 @@
+import time
+from plyer import notification
+
+def notificationn(title, message):
+    notification.notify(
+        title = title,
+        message = message,
+        app_icon = "C:\\Users\\ahmet\\PycharmProjects\\CountdownClockAndTimer\\timer.ico",
+        timeout = 10
+    )
+
+print("""
+******************************
+|                            |  
+|      Geri Sayim Saati      |  
+|            Ve              |
+|        Zamanlayici         |
+|                            |  
+******************************
+""")
+try:
+    t = int(input("Sayac icin saniye degeri gir: "))
+except ValueError:
+    print("Hata: Alfabetik karakter yerine sayi girmelisin.")
+    quit()
+while t:
+    minutes = t // 60
+    second = t % 60
+    timer = "{:02d}:{:02d}".format(minutes, second)
+    print("Kalan sure: ",timer)
+    time.sleep(1)
+    t -= 1
+notificationn("Zaman Doldu","Belirledigin sure bitti. Hoscakal !!!")
